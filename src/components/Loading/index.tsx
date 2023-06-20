96867096c5dd81ec443f2b11c7d8ac8acd5a7ce4
AhmedElGarhy1 <gemater.g@gmail.com>
2023-06-20 21:06:25 +0200

    add the progress loader in loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,45 +1,62 @@
-import { motion } from "framer-motion";
-import { FC } from "react";
+import { useProgress } from "@react-three/drei";
+import { motion, useAnimationControls } from "framer-motion";
+import { FC, useEffect } from "react";
 
-const LoadingVariants = (delay: number) => ({
+const LoadingVariants = {
   initial: {
     height: "100%",
     top: "0%",
   },
   animate: {
     top: "-100%",
-    transition: { delay, duration: 0.8, ease: "easeInOut" },
+    transition: { duration: 0.8, ease: "easeInOut" },
   },
-});
+};
 
-const circleLoadingVariants = (delay: number) => ({
+const circleLoadingVariants = {
   initial: {
     height: "500px",
   },
   animate: {
     height: ["200px", "0px"],
-    transition: { delay, duration: 1, ease: "easeInOut" },
+    transition: { duration: 1, ease: "easeInOut" },
   },
-});
+};
 
 const LoadingComponent: FC = () => {
+  const { progress } = useProgress();
+  const loadingControle = useAnimationControls();
+  const circleControle = useAnimationControls();
+  useEffect(() => {
+    if (progress === 100) {
+      loadingControle.start(LoadingVariants.animate);
+      circleControle.start(circleLoadingVariants.animate);
+      console.log(progress);
+    }
+  }, [progress]);
+
   return (
     <>
       <motion.div
-        variants={LoadingVariants(0.5)}
+        variants={LoadingVariants}
+        animate={loadingControle}
         initial="initial"
-        animate="animate"
         className="fixed w-full">
         <motion.div
           className={
-            "relative bg-[#2435b7] w-full h-full text-white text-center flex justify-center items-center text-6xl"
+            "relative bg-[#2435b7] w-full h-full text-white text-center flex justify-center items-center "
           }>
-          Hello
+          <div>
+            <h1 className="text-6xl">* Hello</h1>
+            <p className="text-lg absolute w-full text-center left-0 z-10 bottom-3 ">
+              {progress} %
+            </p>
+          </div>
           <motion.div
             className="w-[110%]  bg-[#2435b7] rounded-[50%] absolute translate-y-1/2 bottom-0"
-            variants={circleLoadingVariants(0.5)}
+            variants={circleLoadingVariants}
             initial="initial"
-            animate="animate"
+            animate={circleControle}
           />
         </motion.div>
       </motion.div>
